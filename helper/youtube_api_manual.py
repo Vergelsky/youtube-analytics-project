c892bd73641b423e6e45c3954def1e08afcb6a75
Dmitry Vergelsky <graf.werger@gmail.com>
2023-10-09 21:05:44 +0700

    Домашка 1 готова

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -1,5 +1,9 @@
 import json
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
@@ -8,7 +12,7 @@ import isodate
 
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.getenv('YOUTUBE_API_KEY_FOR_E')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,22 @@
+import os
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
+import isodate
+
+load_dotenv()
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YOUTUBE_API_KEY_FOR_E')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+
+        print(self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute())
