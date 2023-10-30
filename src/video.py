e6bacc900ee5720c0bff20c681bd4b090e2ff2be
Dmitry Vergelsky <graf.werger@gmail.com>
2023-10-30 23:56:17 +0700

    Домашка 4 готова.

---
+++ src/video.py
@@ -0,0 +1,28 @@
+from googleapiclient.discovery import build
+import os
+
+
+class Video:
+    api_key: str = os.getenv('YOUTUBE_API_KEY_FOR_E')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, video_id):
+        self.video_id = video_id
+        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                         id=video_id).execute()
+        self.video_title: str = self.video_response['items'][0]['snippet']['title']
+        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        self.video_url: str = f"https://youtu.be/{self.video_response['etag']}"
+
+    def __str__(self):
+        return self.video_title
+
+
+class PLVideo(Video):
+
+    def __init__(self, video_id, pl_id):
+        super().__init__(video_id)
+        self.pl_id = pl_id
+
+
