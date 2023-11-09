768f04027a7a80c33423b4646dff44ca61eff4c9
Dmitry Vergelsky <graf.werger@gmail.com>
2023-11-09 21:37:44 +0700

    Домашка 6 готова.

--- src/video.py
+++ src/video.py
@@ -2,23 +2,30 @@ from googleapiclient.discovery import build
 import os
 
 
-
-
 class Video:
     api_key: str = os.getenv('YOUTUBE_API_KEY_FOR_E')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id):
         self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=video_id).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
-        self.video_url: str = f"https://youtu.be/{self.video_response['etag']}"
+        try:
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=video_id).execute()
+            if not self.video_response["items"]:
+                raise ValueError("Передан неверный ID видео")
+        except ValueError:
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+            self.video_url = None
+        else:
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+            self.video_url: str = f"https://youtu.be/{self.video_response['etag']}"
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
